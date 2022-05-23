9b8b864460f036cd857e1fee89650a8bf548ea4f
Makhambet Torezhan <makhambet.torezhan@jits-4-20.jits.ipa.dc>
2022-05-23 17:04:10 +0600

    edit Dockerfile solution.sh

--- dockerfile/solution.sh
+++ dockerfile/solution.sh
@@ -8,14 +8,17 @@ touch Dockerfile
 
 cat << EOF > ./Dockerfile
 FROM nginx:mainline
+
 RUN rm /etc/nginx/conf.d/*.conf
+
 COPY ./jusan-dockerfile.conf /etc/nginx/conf.d/jusan-dockerfile.conf
+
 COPY ./example/* /var/www/jusan-dockerfile/
 EOF
 
 
 
-sudo docker build -t jusan-dockerfile .
+sudo docker build -t nginx:jusan-dockerfile .
 sudo docker images 
 
 sudo docker run -d -p 6060:80 --name jusan-dockerfile jusan-dockerfile
